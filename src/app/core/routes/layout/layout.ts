d49f636a4bab88a434d390c42433216dc0ed021f
lpdelrio-duonex <luispablo.delrio@duonex.com>
2026-01-27 11:55:57 +0100

    Refactor layout structure to include header, footer, and menu components

--- src/app/core/routes/layout/layout.ts
+++ src/app/core/routes/layout/layout.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutHeader } from './layout-header/layout-header';
+import { LayoutFooter } from './layout-footer/layout-footer';
+import { LayoutMenu } from './layout-menu/layout-menu';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutHeader, LayoutFooter, LayoutMenu],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
