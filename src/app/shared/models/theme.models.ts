53605107b44e67a6d5fdea68ddf38cdecd9b88df
lpdelrio-duonex <luispablo.delrio@duonex.com>
2026-01-30 08:17:39 +0100

    Adding some structural features

---
+++ src/app/shared/models/theme.models.ts
@@ -0,0 +1,1 @@
+export type ThemeColor = 'primary' | 'accent' | 'success' | 'warn' | 'error';
---
+++ src/app/shared/utils/pagination/pagination.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import { Pagination, PaginationFilterType, PaginationValues } from './pagination.models';
+import { HttpParams } from '@angular/common/http';
+import { DateTime } from 'luxon';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PAGINATION_COMMON_PROPERTIES } from 'src/app/shared/utils/pagination/pagination.constants';
+import { HttpService } from 'src/app/core/services/http/http.service';
+import * as R from 'remeda';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaginationService {
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly httpService: HttpService,
+    private readonly router: Router
+  ) {}
+
+  toHttpParams = (pagination: PaginationValues) => {
+    const parameters = new HttpParams({
+      fromObject: R.omit(pagination, ['filters']),
+    });
+    return this.httpService.composeHttpParams(pagination.filters, parameters);
+  };
+
+  fromQueryParams = <T>(
+    definition: Record<string, PaginationFilterType>,
+    values: Partial<PaginationValues> = {}
+  ): Pagination<T> => {
+    const parameters = R.merge(
+      R.pick(this.activatedRoute.snapshot.queryParams, PAGINATION_COMMON_PROPERTIES),
+      R.pick(values, PAGINATION_COMMON_PROPERTIES)
+    );
+    const raw = R.merge(
+      R.omit(this.activatedRoute.snapshot.queryParams, PAGINATION_COMMON_PROPERTIES),
+      R.clone(values.filters)
+    );
+    const filters = this.parseParametersBaseOnDefinition(raw, definition);
+
+    return new Pagination<T>({ ...parameters, filters });
+  };
+
+  updateQueryParams = (value: PaginationValues) => {
+    const parameters = R.pick(value, PAGINATION_COMMON_PROPERTIES);
+    const queryParams = R.merge(parameters, value.filters);
+    this.router.navigate([], { queryParams, relativeTo: this.activatedRoute });
+  };
+
+  private parseParametersBaseOnDefinition = (
+    filters: Record<string, any>,
+    definition: Record<string, PaginationFilterType>
+  ) => {
+    return R.pipe(
+      R.entries(filters),
+      R.map(([key, value]) => {
+        switch (definition[key]) {
+          case 'number': {
+            const num = Number.parseInt(value);
+            return Number.isNaN(num) ? [] : [key, num];
+          }
+          case 'date': {
+            const date = DateTime.fromISO(value);
+            return date.isValid ? [key, date] : [];
+          }
+          case 'boolean': {
+            return [key, value === 'true'];
+          }
+          case 'arrayNumber': {
+            const array = R.isArray(value) ? value : [value];
+
+            const parsed = R.pipe(
+              R.map(array, (v) => Number.parseInt(v)),
+              R.filter(R.isNumber)
+            );
+            return [key, parsed];
+          }
+          case 'arrayString': {
+            return [key, value];
+          }
+          default: {
+            return [key, value];
+          }
+        }
+      }),
+      R.mapToObj(([key, value]) => [key, value])
+    );
+  };
+}
